9546f916e143fb1139f875046f9ab287c1cce599
19859590588 <LAOANshushu123>
2023-07-19 09:00:33 +0800

    动态规划

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args){
-        System.out.println("这是一个测试");
+        int[] test = new int[]{1,0,0,0,1};
+        int num = test.length;
+        int[] temp = new int[num+2];
+        System.arraycopy(test,0,temp,1,test.length);
+        System.out.println(Arrays.toString(test));
+        System.out.println(Arrays.toString(temp));
     }
 }
